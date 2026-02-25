306e8ce0e0f4832574274d4967807a6a01b383d3
Pl4zmatic <Anthony2004david@gmail.com>
2026-02-25 14:33:00 +0100

    -background blur and styling changes, flyto changed

--- js/map.js
+++ js/map.js
@@ -1,5 +1,3 @@
-let mapInitialized = false;
-
 var map = new maplibregl.Map({
     container: 'map',
     style: './js/map_style.json', // stylesheet location
@@ -17,27 +15,31 @@ attributionMap[0].removeAttribute("open")
 attributionMap[0].classList.toggle("maplibregl-compact-show")
 
 const domMap = document.getElementById("map");
-domMap.addEventListener("animationend", () => {
-    map.flyTo({
-        center: [3.73, 51.05],
-        essential: true,
-        zoom: 9,
-        speed: 0.5,
-    });
-    mapInitialized = true;
+domMap.addEventListener("animationend", async () => {
+    await flyTo(map, 3.73, 51.05, 9, 0.5);
+    await flyTo(map, 3.8097, 51.2024, 12, 0.5);
+    await flyTo(map, 3.7745, 51.1372, 9, 0.5);
 })
 
 function round(num, precision) {
     return Number.parseFloat(num).toFixed(precision);
 }
 
-map.on("moveend", () => {
-    if((round(map.getCenter().lng, 2) != round(3.8097, 2) || round(map.getCenter().lat, 2) != round(51.2024, 2)) && mapInitialized) {
-        map.flyTo({
-            center: [3.8097, 51.2024],
+function flyTo(Map, lng, lat, zoom, speed) {
+    return new Promise((resolve, reject) => {
+        Map.flyTo({
+            center: [lng, lat],
             essential: true,
-            zoom: 12,
-            speed: 0.5,
+            zoom: zoom,
+            speed: speed,
         });
-    }
-})
+
+        Map.on("moveend", () => {
+            console.log('move end')
+            if(round(Map.getCenter().lng, 2) == round(lng, 2) && round(Map.getCenter().lat, 2) == round(lat, 2)) {
+                console.log('resolve')
+                resolve();
+            }
+        })
+    })
+}
