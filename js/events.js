bb5f03f94f2bb44fb4fd3f5eab06f684f8da6640
Pl4zmatic <Anthony2004david@gmail.com>
2026-03-10 18:25:12 +0100

    video assests

---
+++ js/events.js
@@ -0,0 +1,4 @@
+const expandKingdomino = document.getElementById("expand-kingdomino");
+const expandPcompose = document.getElementById("expand-pcompose");
+const expandDelaware = document.getElementById("expand-delaware");
+const expandCampusapp = document.getElementById("expand-campusapp");
