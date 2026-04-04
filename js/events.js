1115411f63aadc51931ee5fd8326550f1e3a2e62
Pl4zmatic <Anthony2004david@gmail.com>
2026-04-05 00:50:02 +0200

    play video when in view only

--- js/events.js
+++ js/events.js
@@ -32,9 +32,6 @@ expandButtons.forEach((button) => {
 
     closeButton.addEventListener("click", () => {
         dialog.close();
-        videoElements.forEach((video) => {
-            video.play();
-        });
     });
 });
 
@@ -161,35 +158,76 @@ const sectionButtons = [
     { target: technologiesContainer, button: headerButtonTechnologies },
 ];
 
+const videoKingdomino = document.getElementById("video-child-kingdomino");
+const videoPcompose = document.getElementById("video-child-pcompose");
+const videoDelaware = document.getElementById("video-child-delaware");
+const videoCampusapp = document.getElementById("video-child-campusapp");
+
 const targets = [
-    { target: technologiesContainer, threshold: getIntersectionThreshold(technologiesContainer.clientHeight) },
-    { target: projectsContainer, threshold: getIntersectionThreshold(projectsContainer.clientHeight) },
-    { target: profileContainer, threshold: getIntersectionThreshold(profileContainer.clientHeight) },
+    { target: technologiesContainer, threshold: getIntersectionThreshold(technologiesContainer.clientHeight), callbackFunction: sectionInViewChangeHeader },
+    { target: projectsContainer, threshold: getIntersectionThreshold(projectsContainer.clientHeight), callbackFunction: sectionInViewChangeHeader },
+    { target: profileContainer, threshold: getIntersectionThreshold(profileContainer.clientHeight), callbackFunction: sectionInViewChangeHeader },
+    {
+        target: videoKingdomino,
+        threshold: getIntersectionThreshold(videoKingdomino.clientHeight),
+        callbackFunction: videoInViewChangePlaystate,
+    },
+    {
+        target: videoPcompose,
+        threshold: getIntersectionThreshold(videoPcompose.clientHeight),
+        callbackFunction: videoInViewChangePlaystate,
+    },
+    {
+        target: videoDelaware,
+        threshold: getIntersectionThreshold(videoDelaware.clientHeight),
+        callbackFunction: videoInViewChangePlaystate,
+    },
+    {
+        target: videoCampusapp,
+        threshold: getIntersectionThreshold(videoCampusapp.clientHeight),
+        callbackFunction: videoInViewChangePlaystate,
+    },
 ];
-const observerProfileProject = new IntersectionObserver(changeBackgroundOnEntry, { threshold: targets.map((obj) => obj.threshold) });
-targets.forEach((obj) => observerProfileProject.observe(obj.target));
 
 function getIntersectionThreshold(clientHeight) {
     return Math.min(vh / 2 / clientHeight, 1);
 }
 
-function changeBackgroundOnEntry(entries, observer) {
+const observerProfileProject = new IntersectionObserver(observerFunction, { threshold: targets.map((obj) => obj.threshold) });
+targets.forEach((obj) => observerProfileProject.observe(obj.target));
+
+function observerFunction(entries, observer) {
     entries.forEach((entry, index) => {
-        if (entry.isIntersecting && entry.intersectionRatio > targets.filter((obj) => obj.target == entry.target)[0].threshold) {
-            if (entry.target == profileContainer) {
-                if (!headerButtonPressed) headerButtonProfile.checked = true;
-                // plexus.setTheme("");
-            }
-            if (entry.target == projectsContainer) {
-                if (!headerButtonPressed) headerButtonProjects.checked = true;
-                // plexus.setTheme("background-projects-theme");
-            }
-            if (entry.target == technologiesContainer) {
-                if (!headerButtonPressed) headerButtonTechnologies.checked = true;
-                // plexus.setTheme("background-technologies-theme");
-            }
-        }
+        const target = targets.find((obj) => obj.target == entry.target);
+        target.callbackFunction(entry, target);
     });
+}
+
+function sectionInViewChangeHeader(entry, target) {
+    if (entry.isIntersecting && entry.intersectionRatio > target.threshold) {
+        if (entry.target == profileContainer) {
+            if (!headerButtonPressed) headerButtonProfile.checked = true;
+            // plexus.setTheme("");
+        }
+        if (entry.target == projectsContainer) {
+            if (!headerButtonPressed) headerButtonProjects.checked = true;
+            // plexus.setTheme("background-projects-theme");
+        }
+        if (entry.target == technologiesContainer) {
+            if (!headerButtonPressed) headerButtonTechnologies.checked = true;
+            // plexus.setTheme("background-technologies-theme");
+        }
+    }
+}
+
+function videoInViewChangePlaystate(entry, targetObj) {
+    if (entry.isIntersecting) {
+        targetObj.target.play();
+    }
+
+    if (!entry.isIntersecting) {
+        targetObj.target.pause();
+    }
 }
 
 sectionButtons.forEach((obj) => {
